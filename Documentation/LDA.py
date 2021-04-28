f57885e0408257a4473eef607a10f8f21bb8fea8
Iro Sfoungari <urwsfouggari@gmail.com>
2021-04-28 19:33:24 +0200

    Add files via upload

--- Documentation/LDA.py
+++ Documentation/LDA.py
@@ -6,17 +6,16 @@ Created on Sun Apr 25 12:48:16 2021
 
 # Usual import
 from pprint import pprint
+from pandas import DataFrame
+from sys import exit
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
+import ast
 
-from tqdm import tqdm
 import string
 import os
 import ast
 from pathlib import Path
-from pandas import DataFrame
-
 
 # Gensim
 import gensim
@@ -29,8 +28,8 @@ from gensim.utils import simple_preprocess
 #LDA Mallet
 from gensim.models.wrappers import LdaMallet
 
-os.environ.update({'MALLET_HOME':r'C:/new_mallet/mallet-2.0.8/'})
-mallet_path = r'C:/new_mallet/mallet-2.0.8/bin/mallet.bat'
+#os.environ.update({'MALLET_HOME':r'C:/new_mallet/mallet-2.0.8/'})
+#mallet_path = r'C:/new_mallet/mallet-2.0.8/bin/mallet.bat'
 
 
 fileinput = str(input("Please give the txt file:"))
@@ -43,26 +42,44 @@ fileName = Path(fileinput)
 if fileName.is_file():
     print ("File exists")
     with open(fileName, 'r') as f:
-        mylist = ast.literal_eval(f.read())
+        text_list = ast.literal_eval(f.read())
         fileinput2 = str(input("Please give a directory to save your results:"))
         FilePath = Path(fileinput2)
+        path=str(FilePath)
+        fullpath=str(path +'/')
         if os.path.isdir(FilePath):
            print ("Thanks")
+           path = fullpath + str('/Topics/')
+           path1 = Path(path)
+
+           try:
+               path1.mkdir(parents=True, exist_ok=False)
+           except FileExistsError:
+                  print("Folder is already there")
+           else:
+                print("Folder was created")
+           while True:
+               try:
+                   topic_num = int(input("Enter the number of topics: "))
+                   break
+               except ValueError:
+                   print("Please input integer...")  
+                   continue
         else:
             print ("Directory does not exist! Try again")
             exit()
-
 else:
     print ("This file does not exist!")
+    exit()
 
 
 
 class LDA:
     
-    def __init__(self,mylist):
-        self.mylist=mylist
+    def __init__(self,text_list):
+        self.text_list=text_list
         print("hii")
-        print(len(mylist))
+        print(len(text_list))
 
     def create_dict_corpus(self,text):
          # Create Dictionary
@@ -93,7 +110,7 @@ class LDA:
         return ldamallet
     '''
        
-    def file(self,corpus,lda_model,id_list):
+    def articles_to_topics(self,corpus,lda_model,id_list):
         lda_corpus = lda_model.get_document_topics(corpus)
         k=[doc for doc in lda_corpus]
         k1=list(zip(id_list, k))
@@ -103,11 +120,10 @@ class LDA:
         df['Topic'] = df['Topic'].str.replace("[","").str.replace("(","").str.replace("]","").str.replace(" ","")
         df2= df['Topic'].str.split("\),", expand=True)
         data=pd.concat([df['cord_uid'], df2], axis=1)
-        #data.to_csv(r'C:/Users/Iro Sfoungari/Desktop/sum/blakeies.csv', index = False)
 
         return df,data
    
-    def something(self,data,df,lda_model):
+    def Topics_per_doc(self,data,df,lda_model):
         for x in range(len(data.columns)-1):
             Datas_First_Topic = data[['cord_uid', x]]
             Datas_First_Topic= Datas_First_Topic[x].str.split(",", expand=True)
@@ -120,33 +136,28 @@ class LDA:
             new_df['Topic']=new_df['Topic'].str.replace('"','')
             new_df['Topic']=new_df['Topic'].str.replace(" ","")
             new_df['Topic']=new_df['Topic'].str.replace("+",", ")
-            #new_df["Topic"]=new_df["Topic"].str.replace(", ", ",")
-            new_df.to_csv(r'C:/Users/Iro Sfoungari/Desktop/sum/'+str(x)+'_Topic.csv', index = False)
+            new_df["Topic"]=new_df["Topic"].str.replace(", ", ",")
+            new_df.to_csv(path +str(x)+'_Topic.csv', index = False)
 
 
 
 
 
-First_Instace=LDA(mylist)
-Lemmatized_Text = DataFrame (mylist,columns=['cord_uid','Topic'])
-#print(Lemmatized_Text.head())
+
+Instace=LDA(text_list)
+Lemmatized_Text = DataFrame (text_list,columns=['cord_uid','Topic'])
 id_list=Lemmatized_Text.cord_uid.tolist()
-id2word, corpus= First_Instace.create_dict_corpus(Lemmatized_Text['Topic'])
+id2word, corpus= Instace.create_dict_corpus(Lemmatized_Text['Topic'])
 
 
-k=3
+lda_model=Instace.model(id2word, corpus,topic_num)
+print(lda_model.print_topics(num_topics=topic_num))
 
-lda_model=First_Instace.model(id2word, corpus,k)
-print(lda_model.print_topics(num_topics=k))
+df,data=Instace.articles_to_topics(corpus,lda_model,id_list)
+print(data.head(5))
 
-df,datadata=First_Instace.file(corpus,lda_model,id_list)
-#datadata.T
-print(datadata.head(5))
-
-lenaki=len(datadata)
-
-somehting_new=First_Instace.something(datadata,df,lda_model)
+Instace.Topics_per_doc(data,df,lda_model)
 
 
 
-#C:\Users\Iro Sfoungari\Desktop\sum\all_data_lemmatized.txt
+#C:\Users\Iro Sfoungari\Desktop\sum\Text_for_TE.txt
